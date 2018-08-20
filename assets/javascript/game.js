9dee7522c78ec9f36d96d5e67e9964cb16e5e4ae
Stacey Carrillo <staceycarrillo@Staceys-MacBook-Pro.local>
2018-08-19 21:51:50 -0700

    more js added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,32 @@
 //Create an Array of Words
 
-var phrase = ["Aviators", "Kale"];
+const phrase = ['Aviators', 'Kale'];
 
 //Choose Word Randomly
 
-let randNum = Math.floor(Math.random()*phrase.length);
+let randNum = Math.floor(Math.random() * phrase.length);
 let chosenPhrase = phrase[randNum];
+let underScore = [];
 console.log(chosenPhrase);
 
 
 
 //Create underscore based on length of word
 
+let generateUnderscore = () => {
+    for(let i = 0; i < chosenPhrase.length; i++) {
+    underScore.push('_');
+    }
+    return underScore;
+}
+
+console.log(generateUnderscore());
+
 //Get users guess
+
+document.addEventListener ('keypress', (event) => {
+console.log(event);
+});
 
 //Check if guess is correct
 
