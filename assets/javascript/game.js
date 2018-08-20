5aa423fddbe65d54cdfee3958dbbccc4a1538dd4
Stacey Carrillo <staceycarrillo@Staceys-MacBook-Pro.local>
2018-08-19 21:30:10 -0700

    progress in js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+//Create an Array of Words
+
+var phrase = ["Aviators", "Kale"];
+
+//Choose Word Randomly
+
+let randNum = Math.floor(Math.random()*phrase.length);
+let chosenPhrase = phrase[randNum];
+console.log(chosenPhrase);
+
+
+
+//Create underscore based on length of word
+
+//Get users guess
+
+//Check if guess is correct
+
+//If guess is correct push to the right array
+
+//If wrong push to wrong array
+
