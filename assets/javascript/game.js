2fd6dfc8492b3f7f73b2b380c3b20ffcb09d0997
Stacey Carrillo <staceycarrillo@Staceys-MacBook-Pro.local>
2018-08-23 21:31:50 -0700

    centered bootstrap grid

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,17 @@
 //VARIABLES
 //---------------------------------------
 // How many times a letter can be pressed
-var doubleWord = ['a','b','c',
-				  'd','e','f',
-				  'g','h','i',
-				  'j','k','l',
-				  'm','n','o',
-				  'p','q','r',
-				  's','t','u',
-				  'v','w','x',
-				  'y','z'];
+var doubleWord = ['a', 'b', 'c',
+	'd', 'e', 'f',
+	'g', 'h', 'i',
+	'j', 'k', 'l',
+	'm', 'n', 'o',
+	'p', 'q', 'r',
+	's', 't', 'u',
+	'v', 'w', 'x',
+	'y', 'z'];
 //Word Bank
-var wordBank =['aviators','thrift','kale', 'vintage', 'ironic'];
+var wordBank = ['aviators', 'thrift', 'kale', 'vintage', 'ironic'];
 //Holds currentWord
 var currentWord = "";
 //Holds letters in word
@@ -19,7 +19,7 @@ var lettersInWord = [];
 //Holds number of dashes for word
 var numDashes = 0;
 //Holds Blanks and successful guesses
-var blanksAndSuccesses =[];
+var blanksAndSuccesses = [];
 //Holds Wrong guesses
 var wrongLetters = [];
 //Counters
@@ -29,62 +29,59 @@ var guessesLeft = 9;
 var rightGuessCounter = 0;
 //FUNCTIONS
 //----------------------------------------
-function reset()
-{
+function reset() {
 	//Chooses random word from wordBank
 	currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 	//Splits chosen word into individual letters
 	lettersInWord = currentWord.split('');
 	//Get the number of dashes for current word
 	numDashes = lettersInWord.length;
-	
+
 	//RESET
 	//===========================================================
 	letterGuessed = 0;
 	rightGuessCounter = 0;
 	guessesLeft = 9;
-	wrongLetters =[];
-	blanksAndSuccesses =[];
-	doubleWord = ['a','b','c',
-					  'd','e','f',
-					  'g','h','i',
-					  'j','k','l',
-					  'm','n','o',
-					  'p','q','r',
-					  's','t','u',
-					  'v','w','x',
-					  'y','z'];
-	test=false;
+	wrongLetters = [];
+	blanksAndSuccesses = [];
+	doubleWord = ['a', 'b', 'c',
+		'd', 'e', 'f',
+		'g', 'h', 'i',
+		'j', 'k', 'l',
+		'm', 'n', 'o',
+		'p', 'q', 'r',
+		's', 't', 'u',
+		'v', 'w', 'x',
+		'y', 'z'];
+	test = false;
 	startGame();
 }
-function startGame()
-{
+function startGame() {
 	//Chooses random word from the wordBank
 	currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 	//Splits chosen word into individual letters
 	lettersInWord = currentWord.split('');
 	//Get the number of dashes
 	numDashes = lettersInWord.length;
-	
+
 	//RESET GAME
 	//===========================================================
 	rightGuessCounter = 0;
 	guessesLeft = 9;
-	wrongLetters =[];
-	blanksAndSuccesses =[];
-	doubleWord = ['a','b','c',
-					  'd','e','f',
-					  'g','h','i',
-					  'j','k','l',
-					  'm','n','o',
-					  'p','q','r',
-					  's','t','u',
-					  'v','w','x',
-					  'y','z'];
+	wrongLetters = [];
+	blanksAndSuccesses = [];
+	doubleWord = ['a', 'b', 'c',
+		'd', 'e', 'f',
+		'g', 'h', 'i',
+		'j', 'k', 'l',
+		'm', 'n', 'o',
+		'p', 'q', 'r',
+		's', 't', 'u',
+		'v', 'w', 'x',
+		'y', 'z'];
 
 	//Generate dashes
-	for(var i = 0; i< numDashes; i++)
-	{
+	for (var i = 0; i < numDashes; i++) {
 		blanksAndSuccesses.push('_');
 		document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses;
 	}
@@ -102,58 +99,50 @@ function startGame()
 	console.log(blanksAndSuccesses);
 }
 
-function compareLetters(userKey)
-{
-				console.log('WORKING!');
-				//If user guesses correct letter perform this function 
-				if(currentWord.indexOf(userKey) > -1)
-				{
-					//Loops depending on number of dashes 
-					for(var i = 0; i < numDashes; i++)
-					{
-						//Fills in right index with user guess
-						if(lettersInWord[i] === userKey)
-						{
-							rightGuessCounter++;
-							blanksAndSuccesses[i] = userKey;
-							document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
-						}	
-					}
-					//Testing
-					console.log(blanksAndSuccesses);
-				}
-			//Wrong Keys
-            else
-            {
-                wrongLetters.push(userKey);
-                guessesLeft--;
-                //Changes HTML on page
-                document.getElementById('numGuesses').innerHTML = guessesLeft;
-                document.getElementById('wrongGuesses').innerHTML = wrongLetters.toString().toUpperCase();
-                //Testing
-                console.log('Wrong Letters = ' + wrongLetters);
-                console.log('Guesses left are ' + guessesLeft);
-            }
-			
-			
-		
+function compareLetters(userKey) {
+	console.log('WORKING!');
+	//If user guesses correct letter perform this function 
+	if (currentWord.indexOf(userKey) > -1) {
+		//Loops depending on number of dashes 
+		for (var i = 0; i < numDashes; i++) {
+			//Fills in right index with user guess
+			if (lettersInWord[i] === userKey) {
+				rightGuessCounter++;
+				blanksAndSuccesses[i] = userKey;
+				document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
+			}
+		}
+		//Testing
+		console.log(blanksAndSuccesses);
+	}
+	//Wrong Keys
+	else {
+		wrongLetters.push(userKey);
+		guessesLeft--;
+		//Changes HTML on page
+		document.getElementById('numGuesses').innerHTML = guessesLeft;
+		document.getElementById('wrongGuesses').innerHTML = wrongLetters.toString().toUpperCase();
+		//Testing
+		console.log('Wrong Letters = ' + wrongLetters);
+		console.log('Guesses left are ' + guessesLeft);
+	}
+
+
+
 }
-function winLose()
-{
+function winLose() {
 	// When number dashes if filled with right words then you win
-	if(rightGuessCounter === numDashes)
-	{
+	if (rightGuessCounter === numDashes) {
 		//Counts Wins 
 		winCount++;
 		//Changes HTML on page
 		document.getElementById('winCounter').innerHTML = winCount;
-		var snd = new Audio('service-bell.mp3');    
-snd .onended = function () { alert("I Guess You Won"); };
-snd .play();
+		var snd = new Audio('service-bell.mp3');
+		snd.onended = function () { alert("I Guess You Won"); };
+		snd.play();
 	}
 	// When number of Guesses reaches 0 then You lose
-	else if(guessesLeft === 0)
-	{
+	else if (guessesLeft === 0) {
 		//Counts losses
 		loseCount++;
 		//Changes HTML
@@ -170,16 +159,13 @@ snd .play();
 
 startGame();
 
-	
-document.onkeyup = function(event)
-{
+
+document.onkeyup = function (event) {
 	test = true;
 	var letterGuessed = event.key;
-	for(var i = 0; i < doubleWord.length; i++)
-	{	
-		if(letterGuessed === doubleWord[i] && test === true)
-		{
-			var spliceDword = doubleWord.splice(i,1);
+	for (var i = 0; i < doubleWord.length; i++) {
+		if (letterGuessed === doubleWord[i] && test === true) {
+			var spliceDword = doubleWord.splice(i, 1);
 			//Testing
 			console.log('Double word is = ' + doubleWord[i])
 			console.log('Spliced Word is = ' + spliceDword);
@@ -187,9 +173,8 @@ document.onkeyup = function(event)
 			compareLetters(letterGuessed);
 			winLose();
 		}
-    }	
+	}
 }
 
 
 
-		
